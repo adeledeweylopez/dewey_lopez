bdc51773b598f4e5168e5cefa3c6bde9b575955a
Adele Dewey-Lopez <adeletlopez@gmail.com>
2014-05-31 11:49:20 -0400

    Put in more user actions + admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, 	
+					only: [:index, :edit, :update, :destroy]
+	before_action :signed_out_user, only: [:new, :create]
+	before_action :correct_user,	only: [:edit, :update]
+	before_action :admin_user,		only: :destroy
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
  	def show
 		@user = User.find(params[:id])
 	end
@@ -18,10 +28,50 @@ class UsersController < ApplicationController
 		end
 	end  
 
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user 
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if current_user?(@user)
+			redirect_to root_url
+		else
+			@user.destroy
+			flash[:success] = "User deleted."
+			redirect_to users_url
+		end
+	end
+
 	private
 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, 
 											:password_confirmation)
+		end
+
+		# Before filtersz
+
+		def signed_out_user
+			unless !signed_in?
+				redirect_to root_url
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_url) unless current_user.admin?
 		end
 end
